747c5af6394a7eceb673aeacbf2c4e72df0ce0bf
liuqiang <liuqiang1357@163.com>
2025-04-07 18:33:11 +0800

    chore(eslint): update base.js and types.d.ts for plugin declarations
    - Refactored `base.js` to improve organization of linter options and ensure proper placement of `reportUnusedDisableDirectives`.
    - Enhanced `types.d.ts` by adding detailed type declarations for `eslint-plugin-check-file` and `@next/eslint-plugin-next` to improve type safety and integration.

--- packages/eslint-config/src/base.js
+++ packages/eslint-config/src/base.js
@@ -5,33 +5,29 @@ import prettier from 'eslint-plugin-prettier/recommended';
 import turbo from 'eslint-plugin-turbo';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(
-  {
-    extends: [
-      gitignore(),
-      eslint.configs.recommended,
-      tseslint.configs.recommendedTypeChecked,
-      tseslint.configs.stylisticTypeChecked,
-      turbo.configs['flat/recommended'],
-      prettier,
-    ],
-    plugins: {
-      'check-file': checkFile,
-    },
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
+export default tseslint.config({
+  extends: [
+    gitignore(),
+    eslint.configs.recommended,
+    tseslint.configs.recommendedTypeChecked,
+    tseslint.configs.stylisticTypeChecked,
+    turbo.configs['flat/recommended'],
+    prettier,
+  ],
+  plugins: {
+    'check-file': checkFile,
+  },
+  languageOptions: {
+    parserOptions: {
+      projectService: {
+        allowDefaultProject: ['*.js'],
       },
     },
-    linterOptions: {
-      reportUnusedDisableDirectives: 'error',
-    },
-    rules: {
-      'check-file/filename-naming-convention': ['error', { '**/*.{js,jsx,ts,tsx}': '[0-9a-z-.]+' }],
-    },
   },
-  {
-    files: ['**/*.{js,jsx}'],
-    extends: [tseslint.configs.disableTypeChecked],
+  linterOptions: {
+    reportUnusedDisableDirectives: 'error',
   },
-);
+  rules: {
+    'check-file/filename-naming-convention': ['error', { '**/*.{js,jsx,ts,tsx}': '[0-9a-z-.]+' }],
+  },
+});
--- packages/eslint-config/src/types.d.ts
+++ packages/eslint-config/src/types.d.ts
@@ -1,3 +1,18 @@
-declare module 'eslint-plugin-check-file';
+declare module 'eslint-plugin-check-file' {
+  const plugin: Record<string, unknown>;
+  export default plugin;
+}
 
-declare module '@next/eslint-plugin-next';
+declare module '@next/eslint-plugin-next' {
+  import type { TSESLint } from '@typescript-eslint/utils';
+
+  type ESLintRules = Record<string, TSESLint.Linter.RuleEntry>;
+
+  const plugin: TSESLint.Linter.Plugin & {
+    configs: {
+      recommended: { rules: ESLintRules };
+      'core-web-vitals': { rules: ESLintRules };
+    };
+  };
+  export default plugin;
+}
