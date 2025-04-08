76a1d96a16aa244a2252d1ffde96aee1441c3137
liuqiang <liuqiang1357@163.com>
2025-04-08 17:22:43 +0800

    chore(dependencies): remove unused eslint-plugin-react-compiler and clean up pnpm-lock.yaml

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -21,7 +21,6 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning>
       <body className={`${fontSans.variable} ${fontMono.variable} font-sans antialiased`}>
         <Providers>{children}</Providers>
-        <span></span>
       </body>
     </html>
   );
--- packages/eslint-config/src/react.js
+++ packages/eslint-config/src/react.js
@@ -1,22 +1,25 @@
 import * as queryPlugin from '@tanstack/eslint-plugin-query';
 import react from 'eslint-plugin-react';
-import compiler from 'eslint-plugin-react-compiler';
 import hooks from 'eslint-plugin-react-hooks';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config({
-  // @ts-expect-error - queryPlugin.default exists at runtime but type definition is incorrect
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  extends: [queryPlugin.default.configs['flat/recommended']],
+  extends: [
+    react.configs.flat.recommended,
+    react.configs.flat['jsx-runtime'],
+    // @ts-expect-error - queryPlugin.default exists at runtime but type definition is incorrect
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    queryPlugin.default.configs['flat/recommended'],
+  ],
   plugins: {
-    react,
-    'react-compiler': compiler,
     'react-hooks': hooks,
   },
   rules: {
     ...react.configs['jsx-runtime'].rules,
     ...hooks.configs.recommended.rules,
 
-    'react-compiler/react-compiler': 'error',
+    'react/prop-types': 'off',
+    'react/self-closing-comp': 'error',
   },
+  settings: { react: { version: 'detect' } },
 });
